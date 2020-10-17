e0140cf54f113bd3b45960c1940c2536e97ea64c
cezklosowski <cez.klosowski@gmail.com>
2020-10-17 15:42:34 +0200

    newBooks, deleteBooks

--- src/main/java/com/cezklosowski/module/books/BooksController.java
+++ src/main/java/com/cezklosowski/module/books/BooksController.java
@@ -1,9 +1,7 @@
 package com.cezklosowski.module.books;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -15,11 +13,21 @@ public class BooksController {
 
     @GetMapping("/rest/books/{id}")
     public BooksEntity getBook(@PathVariable Long id){
-        return booksRepository.getOne(id);
+        return booksRepository.findById(id).get();
     }
 
     @GetMapping("/rest/books")
     public List<BooksEntity> getBooks() {
         return booksRepository.findAll();
+    }
+
+    @PostMapping("/rest/books")
+    public BooksEntity newBooks(@RequestBody BooksEntity booksEntity) {
+        return booksRepository.saveAndFlush(booksEntity);
+    }
+
+    @DeleteMapping("/rest/books/{id}")
+    public void deleteBooks(@PathVariable Long id) {
+        booksRepository.deleteById(id);
     }
 }
