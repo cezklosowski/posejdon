67da98996b6ef5bd67703b3df26543c0aea6ec60
cezklosowski <cez.klosowski@gmail.com>
2020-10-17 10:26:29 +0200

    home module

---
+++ src/main/java/com/cezklosowski/module/home/HomeController.java
@@ -0,0 +1,19 @@
+package com.cezklosowski.module.home;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+
+    @Autowired
+    private HomeService homeService;
+
+    @RequestMapping("/")
+    public String getHome(Model model){
+        model.addAttribute("greetings", homeService.getGreetings());
+        return "home";
+    }
+}
